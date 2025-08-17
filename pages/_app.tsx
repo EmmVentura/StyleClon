75e58b987a270659c62f73602dccd95e0346c121
ventura <jesussolis2184@gmail.com>
2025-08-17 16:51:36 -0600

    cambios menores

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { ThemeProvider } from "next-themes";
+import { motion, AnimatePresence } from "framer-motion";
 
 
 export default function App({ Component, pageProps }: AppProps) {
